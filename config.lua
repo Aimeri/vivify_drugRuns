7f9fb5c6f65066e616136590e71ff52e86880d61
Aimeri <edntremblay@gmail.com>
2025-01-19 23:03:47 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.PedLocation = vector4(-340.8, -984.35, 30.32, 251.21)
-Config.PedModel = 'JessePinkman'
+Config.PedModel = 'a_f_m_business_02'
 
 Config.StartMoney = 1000
 Config.StartItem = 'strange_token'
@@ -21,14 +21,14 @@ Config.RewardItem = 'oxy'
 Config.GlobalCooldown = 60
 
 Config.DropSpots = {
-    vector3(-346.95, -1015.52, 30.38),
-    vector3(-325.8, -1022.23, 30.39),
-    vector3(-320.67, -1009.41, 30.39),
-    vector3(-308.11, -1005.68, 30.39),
-    vector3(-294.81, -1007.49, 30.38),
-    vector3(-280.58, -1011.97, 30.38),
-    vector3(-283.54, -1025.62, 30.38),
-    vector3(-296.54, -1021.57, 30.38),
-    vector3(-294.07, -1008.72, 30.38),
-    vector3(-308.95, -1011.41, 30.39)
+    vector3(-592.33, -1162.0, 22.32),
+    vector3(-477.48, -594.12, 31.17),
+    vector3(-70.17, -1198.08, 27.67),
+    vector3(78.76, -1901.58, 21.72),
+    vector3(471.38, -1275.99, 29.56),
+    vector3(973.08, -195.71, 73.21),
+    vector3(118.19, 324.17, 112.14),
+    vector3(-146.5, 229.5, 94.93),
+    vector3(-1816.81, -410.75, 44.86),
+    vector3(-1551.62, -439.37, 35.88)
 }
